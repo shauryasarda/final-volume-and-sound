e9019d633d96ce13f66c2658eda8b640b6277bb8
shauryasarda <98157252+shauryasarda@users.noreply.github.com>
2024-01-30 19:36:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload()
+{
+     song = loadSound("music.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.posenet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modleLoaded() {
+    console.log('PoseNet Is Initialoized');
+}
+
+function gotPoses(results)
+    {
+        if(results.length > 0)
+        {
+            console.log(results);
+            leftWristX = results[0].pose.leftWrist.x;
+            leftWristY = results[0].pose.leftWrist.y;
+            console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY)
+
+            rightWristX = results[0].pose.rightWrist.x;
+            rightWristY = results[0].pose.rightWrist.y;
+            console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY)
+        }
+    }
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    son.rated(1);
+}
